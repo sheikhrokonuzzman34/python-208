b3792e90cc9dd20845eb5db250bc20d43f2a0a41
Soyebmahir <tssoyeb@gmail.com>
2024-01-06 11:27:42 +0600

    first commit

---
+++ def.py
@@ -0,0 +1,43 @@
+
+
+# def add(a,b):
+#     sum = a+b
+#     print(sum)
+
+
+# add()
+# add(15,20)
+# add(10,30)
+
+
+# x = lambda a: a + 10
+# print(x(10))
+
+
+# def my_function(fname, lname):
+#   print(fname + " " + lname)
+
+# my_function("Emil", "Rokon")
+
+
+# def my_function(*kids):
+#   print("The youngest child is " + kids[0])
+
+# my_function("Emil", "Tobias", "Linus")
+
+
+# def my_function(child3, child2, child1):
+#   print("The youngest child is " + child3)
+
+# my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+# my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+# my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
